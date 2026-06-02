4bb67f169b98f29c9f1157db73c0c3a9f4df4a8a
kaitheone <cesarkayomacoma@gmail.com>
2026-06-01 21:19:54 -0300

    feat: update index routes to return HTML content for clientes and dashboard

--- app/routes/clientes.py
+++ app/routes/clientes.py
@@ -5,4 +5,17 @@ bp = Blueprint("clientes", __name__)
 
 @bp.route("/")
 def index():
-    return "clientes ok"
+    return """
+    <html>
+        <head>
+            <title>Clientes</title>
+        </head>
+        <body>
+            <h1>ShieldFort - Clientes</h1>
+            <p> Carlos Reis carlos.reis@gmail.com</p>
+            <p> Guilherme Dantas guilherme.dantas@gmail.com</p>
+            <p> Roseanne Maia roseannemaia@gmail.com</p>
+            <p> Tereza Dias tereza.dias@gmail.com</p>
+        </body>
+    </html>
+    """
--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -5,4 +5,17 @@ bp = Blueprint("dashboard", __name__)
 
 @bp.route("/")
 def index():
-    return "dashboard ok"
+    return """
+    <html>
+        <head>
+            <title>Dashboard</title>
+        </head>
+        <body>
+            <h1>ShieldFort - Home</h1>
+            <p>Serviços Realizados 20</p>
+            <p>Valor Faturado R$ 3.697,50</p>
+            <p>Total Clientes 16</p>
+            <p>Vendas Mês Atual 18</p>
+        </body>
+    </html>
+    """
